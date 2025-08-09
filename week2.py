94686dae908ce803325797d6905c988a344f6561
Peter Karuri <163239853+Sterlingbot65@users.noreply.github.com>
2025-08-09 05:35:43 +0300

    Add files via upload

---
+++ week2.py
@@ -0,0 +1,12 @@
+my_list=[]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list[1]=15
+my_list2=[50,60,70]
+my_list.extend(my_list2)
+my_list.remove(40)
+my_list.sort(reverse=True)
+inde=my_list.index(30)
+print(inde)
